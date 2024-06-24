ad79d9bced4b63dba5fdf0844fe9956953023577
rodrigo.carneiro <rodrigo.carneiro@benner.com.br>
2024-06-24 18:31:59 -0300

    add sqlite

---
+++ BibliaCriarDB/BibliaCriarDB/DalHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliaCriarDB
+{
+    public class DalHelper
+    {
+        private const string SQLITE_FILENAME = @"c:\Git\BibliaOnline\Biblia.sqlite";
+
+        private SQLiteConnection DbConnection()
+        {
+            var cnn = new SQLiteConnection("Data Source=" + SQLITE_FILENAME + "; Version=3;");
+            cnn.Open();
+            return cnn;
+        }
+
+        public void CriarDB()
+        {
+            SQLiteConnection.CreateFile(SQLITE_FILENAME);
+        }
+    }
+}
